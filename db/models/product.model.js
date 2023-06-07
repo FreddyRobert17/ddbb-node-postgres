2cd5cb9b0b134e90620693fa889b5e1f8dad6fe8
FreddyRobert17 <freddy.bedregal.ticona@gmail.com>
2023-06-07 11:01:35 -0500

    First commit

---
+++ db/models/product.model.js
@@ -0,0 +1,51 @@
+const { Model, DataTypes, Sequelize } = require('sequelize');
+
+const PRODUCT_TABLE = 'products';
+
+const ProductSchema = {
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: DataTypes.INTEGER,
+  },
+  name: {
+    allowNull: false,
+    type: DataTypes.STRING,
+  },
+  image: {
+    allowNull: false,
+    type: DataTypes.STRING,
+  },
+  description: {
+    allowNull: false,
+    type: DataTypes.TEXT,
+  },
+  price: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  createdAt: {
+    allowNull: false,
+    type: DataTypes.DATE,
+    field: 'created_at',
+    defaultValue: Sequelize.NOW,
+  },
+};
+
+class Product extends Model {
+  static associate(models) {
+    //this.hasMany()
+  }
+
+  static config(sequelize) {
+    return {
+      sequelize,
+      tableName: PRODUCT_TABLE,
+      modelName: 'Product',
+      timestamps: false,
+    };
+  }
+}
+
+module.exports = { Product, ProductSchema, PRODUCT_TABLE };
---
+++ services/customers.service.js
@@ -0,0 +1,42 @@
+const boom = require('@hapi/boom');
+
+const { models } = require('../libs/sequelize');
+
+class CustomerService {
+  constructor() {}
+
+  async find() {
+    const response = await models.Customer.findAll();
+    return response;
+  }
+
+  async findOne(id) {
+    const user = await models.Customer.findByPk(id);
+    if (!user) {
+      throw boom.notFound('Customer not found');
+    }
+    return user;
+  }
+
+  async create(data) {
+    console.log(data, 'FREEEE');
+    const newCustomer = await models.Customer.create(data);
+    return newCustomer;
+  }
+
+  async update(id, changes) {
+    const model = await this.findOne(id);
+    const response = await model.update(changes);
+    return response;
+  }
+
+  async delete(id) {
+    const model = await this.findOne(id);
+    await model.destroy();
+    return {
+      response: true,
+    };
+  }
+}
+
+module.exports = CustomerService;
